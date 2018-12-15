2cb5135a6c7e94c6298a882cd9c1439f5b900708
SHEFFmachine <sheff@sheffmachine.com>
2018-12-15 12:12:18 -0800

    Implement mkdir and ls commands in the filesystem

---
+++ src/lectures/commands/Ls.scala
@@ -0,0 +1,19 @@
+package lectures.commands
+import lectures.FileSystem.State
+import lectures.files.DirEntry
+
+class Ls extends Command {
+  override def apply(state: State): State = {
+    val contents = state.wd.contents
+    val niceOutput = createNiceOutput(contents)
+    state.setMessage(niceOutput)
+  }
+
+  def createNiceOutput(contents: List[DirEntry]): String = {
+    if (contents.isEmpty) ""
+    else {
+      val entry = contents.head
+      entry.name + "[" + entry.getType + "]" + "\n" + createNiceOutput(contents.tail)
+    }
+  }
+}
--- src/lectures/files/DirEntry.scala
+++ src/lectures/files/DirEntry.scala
@@ -3,4 +3,5 @@ package lectures.files
 abstract class DirEntry(val parentPath: String, val name: String) {
   def path: String = parentPath + Directory.SEPARATOR + name
   def asDirectory: Directory
+  def getType: String
 }
